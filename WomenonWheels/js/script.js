a5d48e23c26498672c40043b30ec05bbfe9c1c1d
Malaika Jawed <136737585+malaikaja2@users.noreply.github.com>
2026-09-16 20:05:05 +0500

    Delete WomenonWheels directory

--- WomenonWheels/js/script.js
+++
@@ -1,3 +0,0 @@
-function showAuthButtons() {
-    document.getElementById("authButtons").style.display = "block";
-}
--- WomenonWheels/php/sos_table_updates.sql
+++
@@ -1,4 +0,0 @@
-USE women_on_wheels_db;
-
-ALTER TABLE sos_alerts
-  ADD INDEX idx_sos_alerts_role_status_created (role, status, created_at);
